b5233061c309d42c980070bbab4a7688b87530fc
8amjoker404 <8amjoker@gmail.com>
2026-06-02 07:50:55 -0700

    files update

--- frontend/src/api/client.js
+++ frontend/src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const apiClient = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:4000',
+  baseURL:'https://api.dbms.copupbid.com',
   headers: {
     'Content-Type': 'application/json',
   },
